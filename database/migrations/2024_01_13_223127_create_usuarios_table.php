2203cc9be3afb1bc2628e4705ee9a9169fd7e09d
nvivas <moeshe@gmail.com>
2024-01-26 08:07:09 +0100

    commit inicial

---
+++ database/migrations/2024_01_13_223127_create_usuarios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    // En el archivo create_usuarios_table.php
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->string('email')->unique();
+            $table->string('contraseña')->unique();
+            $table->string('dirección')->unique();
+            $table->string('codigoPostal')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
