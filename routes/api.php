b24384e0e1178433b930dce5bb93f2062dfa63a3
nvivas <moeshe@gmail.com>
2024-02-05 05:35:05 +0100

    conexión con Angular

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,9 +11,9 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+ */
 
-Route::get('api/juegos', 'App\Http\Controllers\JuegoController@index');
+Route::group(['middleware' => ['cors']], function () {
+    //Rutas a las que se permitirá acceso
+    Route::get('juegos', 'App\Http\Controllers\JuegoController@index');
+});
