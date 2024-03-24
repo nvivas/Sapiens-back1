405131500a6fd181b439996e5d777b53afe61a5b
nvivas <moeshe@gmail.com>
2024-03-24 10:28:13 +0100

    Feature/stock (#1)
    * cambio de stock
    
    * comentado añadir juegos

--- app/Http/Controllers/JuegoController.php
+++ app/Http/Controllers/JuegoController.php
@@ -33,21 +33,35 @@ class JuegoController extends Controller
 
 
     // Este método es para añadir juegos. De momento no funciona en el front
-    public function store(Request $request)
-    {
-        $juego = new Juego();
-        $juego->nombre = $request->input('nombre');
-        $juego->descripcion = $request->input('descripcion');
-        $juego->precio = $request->input('precio');
-        $juego->categoria = $request->input('categoria');
-        $juego->stock = $request->input('stock');
-        if ($request->hasFile('imagen')) {
-            $imagenNombre = $request->file('imagen')->getClientOriginalName();
-            $request->file('imagen')->storeAs('public/imagenes', $imagenNombre);
-            $juego->imagen = $imagenNombre;
-        }
-        $juego->save();
+    // public function store(Request $request)
+    // {
+    //     $juego = new Juego();
+    //     $juego->nombre = $request->input('nombre');
+    //     $juego->descripcion = $request->input('descripcion');
+    //     $juego->precio = $request->input('precio');
+    //     $juego->categoria = $request->input('categoria');
+    //     $juego->stock = $request->input('stock');
+    //     if ($request->hasFile('imagen')) {
+    //         $imagenNombre = $request->file('imagen')->getClientOriginalName();
+    //         $request->file('imagen')->storeAs('public/imagenes', $imagenNombre);
+    //         $juego->imagen = $imagenNombre;
+    //     }
+    //     $juego->save();
 
-        return response()->json(['message' => 'Juego creado exitosamente'], 201);
+    //     return response()->json(['message' => 'Juego creado exitosamente'], 201);
+    // }
+
+    public function actualizarStock(Request $request) {
+        $juegoId = $request->input('juego_id');
+        $cantidadComprada = $request->input('cantidad_comprada');
+
+        $juego = Juego::find($juegoId);
+        if ($juego) {
+            $juego->stock -= $cantidadComprada;
+            $juego->save();
+            return response()->json(['message' => 'Stock actualizado correctamente'], 200);
+        } else {
+            return response()->json(['error' => 'Juego no encontrado'], 404);
+        }
     }
 }
